199eb486808b25139c91f33af72f46707fa8d983
sanketsirsikar <sanketsirsikar2@gmail.com>
2022-11-04 14:50:33 +0530

    welcome to Linklist program

--- src/com/bridgelabz/LinkListMain.java
+++ src/com/bridgelabz/LinkListMain.java
@@ -1,2 +1,7 @@
-package com.bridgelabz;public class LinkListMain {
+package com.bridgelabz;
+
+public class LinkListMain {
+    public static void main (String [] args ){
+        System.out.println("Welcome to Linklist  program ");
+    }
 }
